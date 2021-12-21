0c7889d9a28ee0eda9b0c30df46105159ce543dd
manasseh <hirekaanm3@gmail.com>
2021-12-21 11:51:08 +0000

    test

---
+++ mysite/ads/forms.py
@@ -0,0 +1,42 @@
+from django import forms
+from .models import Ads
+from django.core.files.uploadedfile import InMemoryUploadedFile
+from .humanize import naturalsize
+
+class CreateForm(forms.ModelForm):
+    max_upload_limit = 2*1024*1024
+    max_upload_limit_text = naturalsize(max_upload_limit)
+
+
+    picture = forms.FileField(required=False, label="File to Upload <= "+ max_upload_limit_text)
+    upload_field_name = 'picture'
+
+    class Meta:
+        model = Ads
+        fields = ['title','text','picture']
+
+    #validate the size of the uploaded picture file
+    def clean(self):
+        #get the cleaned data
+        cleaned_data = super().clean()
+        pic = cleaned_data.get('picture')
+        if pic is None: return
+        if len(pic) > self.max_upload_limit:
+            self.adderror('picture', 'File must be < ' + self.max_upload_limit_text +'bytes')
+
+    #Convert upload file object to a picture
+    def save(self, commit=True):
+        instance = super(CreateForm, self).save(commit=False)
+
+        f = instance.picture
+        if isinstance(f, InMemoryUploadedFile):
+            bytearr = f.read()
+            instance.content_type = f.content_type
+            instance.picture = bytearr
+
+        if commit:
+            instance.save()
+
+        return instance
+
+
